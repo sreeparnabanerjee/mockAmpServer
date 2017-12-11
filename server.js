f64db80560aaf6b3e7bc5a9c777a073c5b1ba3e0
sreeparna <sbanerjee@cleo.com>
2017-12-11 13:02:07 +0530

    adding configurable hostname

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 var port = process.env.PORT || 8081;
+var hostname = process.env.HOSTNAME || 'localhost';
 
 var routes = require('./lib/routes/ampRoutes'); //importing route
 routes(app); //register the route
 
-app.listen(port);
+app.listen(port, hostname);
 console.log('AMP mock server started on: ' + port);
